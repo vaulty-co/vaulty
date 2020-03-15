7d83636aa04ba15a9c0e3c755ebc599556d86225
Pavel Gabriel <alovak@gmail.com>
2020-03-15 10:25:03 +0300

    remove commented code

--- main.go
+++ main.go
@@ -15,55 +15,12 @@ func errResponse(r *http.Request, message string) *http.Response {
 		message)
 }
 
-// func transformRequestBody(req *http.Request) error {
-
-// 	tr = transformer.NewRequestBodyTransformer(req)
-// 	tr.TransformRequestBody()
-
-// 	err := transformBody(req)
-// 	err := transformBody(req)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	return nil
-
-// send body to sidekiq
-// client := redis.NewClient(&redis.Options{
-// 	Addr:     os.Getenv("REDIS_URL"),
-// 	Password: "", // no password set
-// 	DB:       0,  // use default DB
-// })
-
-// pubsub, err := client.Subscribe("mychannel")
-
-// wait for sidekiq response
-// read modified body
-// update request body with new one
-
-// client.LPush("queue:default", "{\"queue\":\"default\",\"class\":\"ProxyWorker::Worker\",\"args\":\"{}\",\"jid\":\"ecbdb1e0927b71c5228839e\",\"enqueued_at\":1584172882.16241,\"at\":1584172882.16241}")
-
-// body, err := ioutil.ReadAll(req.Body)
-// if err != nil {
-// 	return err
-// }
-
-// oldBody := string(body)
-// newBody := " + new body is here!!!"
-
-// buf2 := bytes.NewBufferString(oldBody + newBody)
-// req.Header.Del("Content-Length")
-// req.ContentLength = int64(buf2.Len())
-// req.Body = ioutil.NopCloser(bufio.NewReader(buf2))
-// return nil
-// }
-
 func main() {
 	proxy := goproxy.NewProxyHttpServer()
 	proxy.OnRequest().HandleConnect(goproxy.AlwaysMitm)
 	proxy.OnRequest().DoFunc(func(req *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
 		tr := transformer.NewRequestBodyTransformer(req)
 		err := tr.TransformRequestBody()
-
 		if err != nil {
 			return nil, errResponse(req, err.Error())
 		}

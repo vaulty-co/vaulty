e1f52ef0dd1bc12573e59477e658a85e1d6e94bb
Pavel Gabriel <alovak@gmail.com>
2020-04-01 20:08:42 +0300

    remove debug info

--- transformer/test_transformer/test_transformer.go
+++ transformer/test_transformer/test_transformer.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"bytes"
 	"io/ioutil"
-	"log"
 	"net/http"
 
 	"github.com/vaulty/proxy/transformer"
@@ -18,8 +17,6 @@ func NewTransformer() transformer.Transformer {
 }
 
 func (t *Transformer) TransformRequestBody(routeID string, req *http.Request) error {
-	log.Printf("Transforming request body for route: %s", routeID)
-
 	b, _ := ioutil.ReadAll(req.Body)
 	body := string(b)
 
@@ -31,16 +28,12 @@ func (t *Transformer) TransformRequestBody(routeID string, req *http.Request) er
 	req.Header.Del("Content-Length")
 	req.ContentLength = size
 
-	log.Println("Done")
-
 	return nil
 }
 
 func (t *Transformer) TransformResponseBody(routeID string, res *http.Response) error {
 	b, _ := ioutil.ReadAll(res.Body)
 	body := string(b)
-
-	log.Printf("Transforming response body for route: %s", routeID)
 
 	body += " transformed"
 
@@ -49,8 +42,6 @@ func (t *Transformer) TransformResponseBody(routeID string, res *http.Response) 
 	size := int64(len(body))
 	res.Header.Del("Content-Length")
 	res.ContentLength = size
-
-	log.Println("Done")
 
 	return nil
 }

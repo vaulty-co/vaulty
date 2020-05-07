b30340c25c418edc56d34081f512ce2c4d0456fd
Pavel Gabriel <alovak@gmail.com>
2020-05-07 20:13:32 +0300

    load routes into inmem storage

--- core/config.go
+++ core/config.go
@@ -1,6 +1,7 @@
 package core
 
 import (
+	"io"
 	"os"
 
 	"github.com/kelseyhightower/envconfig"
@@ -12,7 +13,8 @@ type Configuration struct {
 	Environment   string `yaml:"environment" envconfig:"PROXY_ENV"`
 	BaseHost      string `yaml:"base_host" envconfig:"BASE_HOST"`
 	ProxyPassword string `yaml:"proxy_pass" envconfig:"PROXY_PASS"`
-	CaPath        string `yaml:"ca_path" envconfig:"CA_PATH"`
+	RoutesFile    string `default:"~/.vaulty/routes.json" yaml:"routes_file" envconfig:"ROUTES_FILE"`
+	CaPath        string `default:"~/.vaulty" yaml:"ca_path" envconfig:"CA_PATH"`
 	Redis         struct {
 		URL string `yaml:"url" envconfig:"REDIS_URL"`
 	}
@@ -23,25 +25,11 @@ var Config *Configuration
 func LoadConfig(file string) *Configuration {
 	Config = &Configuration{}
 
-	setDefaults(Config)
 	readFile(file, Config)
 	readEnv(Config)
+	expandPaths(Config)
 
 	return Config
-}
-
-func setDefaults(cfg *Configuration) {
-	var err error
-
-	cfg.CaPath, err = homedir.Expand("~/.vaulty")
-	if err != nil {
-		panic(err)
-	}
-
-	cfg.RoutesFile, err = homedir.Expand("~/.vaulty/routes.json")
-	if err != nil {
-		panic(err)
-	}
 }
 
 func readFile(file string, cfg *Configuration) {
@@ -53,13 +41,27 @@ func readFile(file string, cfg *Configuration) {
 
 	decoder := yaml.NewDecoder(f)
 	err = decoder.Decode(cfg)
-	if err != nil {
+	if err != nil && err != io.EOF {
 		panic(err)
 	}
 }
 
 func readEnv(cfg *Configuration) {
 	err := envconfig.Process("", cfg)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func expandPaths(cfg *Configuration) {
+	var err error
+
+	cfg.CaPath, err = homedir.Expand(cfg.CaPath)
+	if err != nil {
+		panic(err)
+	}
+
+	cfg.RoutesFile, err = homedir.Expand(cfg.RoutesFile)
 	if err != nil {
 		panic(err)
 	}

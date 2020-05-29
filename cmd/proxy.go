5009f7aa380d7c2860a0a9bc93c19b0a1e3be1b1
Pavel Gabriel <alovak@gmail.com>
2020-05-29 16:42:30 +0300

    fix aliases for CLI flags

--- cmd/proxy.go
+++ cmd/proxy.go
@@ -3,11 +3,8 @@ package main
 import (
 	"fmt"
 
-	log "github.com/sirupsen/logrus"
-	prefixed "github.com/x-cray/logrus-prefixed-formatter"
-
 	"github.com/urfave/cli/v2"
-	vaulty "github.com/vaulty/vaulty"
+	"github.com/vaulty/vaulty"
 )
 
 var config = vaulty.NewConfig()
@@ -17,41 +14,42 @@ var proxyCommand = &cli.Command{
 	Usage: "run proxy server",
 	Flags: []cli.Flag{
 		&cli.StringFlag{
-			Name:        "address, a",
+			Name:        "address",
+			Aliases:     []string{"a"},
 			Value:       ":8080",
 			Usage:       "address that vaulty should listen on",
 			Destination: &config.Address,
 		},
 		&cli.StringFlag{
-			Name:        "routes-file, r",
+			Name:        "routes-file",
+			Aliases:     []string{"r"},
 			Value:       "./routes.json",
 			Usage:       "routes file",
 			Destination: &config.RoutesFile,
 		},
 		&cli.StringFlag{
-			Name:        "ca-path, ca",
+			Name:        "ca-path",
+			Aliases:     []string{"ca"},
 			Value:       "./",
 			Usage:       "path to CA key and cert",
 			Destination: &config.CAPath,
 		},
 		&cli.StringFlag{
-			Name:        "proxy-pass, p",
+			Name:        "proxy-pass",
+			Aliases:     []string{"p"},
 			Usage:       "forward proxy password",
 			EnvVars:     []string{"PROXY_PASS"},
 			Destination: &config.ProxyPassword,
 		},
 		&cli.StringFlag{
-			Name:        "key, k",
+			Name:        "key",
+			Aliases:     []string{"k"},
 			Usage:       "forward proxy password",
 			EnvVars:     []string{"ENCRYPTION_KEY"},
 			Destination: &config.EncryptionKey,
 		},
 	},
 	Action: func(c *cli.Context) error {
-		log.SetFormatter(&prefixed.TextFormatter{
-			FullTimestamp: true,
-		})
-
 		if err := config.GenerateMissedValues(); err != nil {
 			return fmt.Errorf("Error with generating missed values: %s", err)
 		}
